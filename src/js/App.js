998f3d2dc7e2f01fd1c020d17e83cc7aa845a0c1
melissa <nadia.mm.mariduena@gmail.com>
2020-06-09 23:42:56 +0200

    second try react 1 lesson

---
+++ src/js/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+//import logo from "../svg/logo.svg";
+
+function App() {
+  return (
+    // <div className="App">
+    // <header className="App-header">
+    <div>
+      <p>Hello this is my second React beginner test</p>
+      <img src="./img/Gucci-Pre-Fall-2020-metalmagazine-12.jpg" alt="girl" />
+    </div>
+    // </header>
+    // </div>
+  );
+}
+
+export default App;
